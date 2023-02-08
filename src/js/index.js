cff1d49d77fa4d3f3cac009303a3c3ae4c03345b
João Victor <bito.jvss@gmail.com>
2023-02-07 22:52:14 -0300

    Site do filme do mário

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+/*
+1- qndo o usuario clicar no botão de ver trailer, devemos abrir a modal com o video do trailer
+
+2- qndo o usuario clicar no x, devemos fechar a modal
+*/
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+	modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+	alternarModal();
+	video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+	alternarModal();
+	video.setAttribute("src", "");
+});
